125abdb4bd746c7145a0b67211c3ee1c4ac8cf05
Marcelo <mlsilva@mlsilva.local>
2023-01-05 23:01:46 -0300

    Testes de deploy dokku

--- src/main/java/foundation/cmo/sales/services/SalesService.java
+++ src/main/java/foundation/cmo/sales/services/SalesService.java
@@ -8,9 +8,16 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Service
 @GraphQLApi
 public class SalesService {
-	
+
 	private final String DES_QUERY_TEST = "Query para testar se o serviço está disponível.";
-	
+
+	/**
+	 * The {@code String} class represents character strings. All string literals in
+	 * Java programs, such as {@code "abc"}, are implemented as instances of this
+	 * class.
+	 * 
+	 * @return
+	 */
 	@GraphQLQuery(name = "test", description = DES_QUERY_TEST)
 	public String testService() {
 		return "OK";
