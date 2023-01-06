3570dea355316ffb41fca6ddddc2084b6f09247f
Marcelo <mlsilva@mlsilva.local>
2023-01-06 00:57:12 -0300

    Testes de deploy dokku

--- src/main/java/foundation/cmo/sales/models/Product.java
+++ src/main/java/foundation/cmo/sales/models/Product.java
@@ -1,19 +1,28 @@
 package foundation.cmo.sales.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.leangen.graphql.annotations.GraphQLQuery;
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class Product {
 	@GraphQLQuery(name = "cd_ncm")
+	@JsonProperty(value = "Ncm")
 	private String ncm;
 	
 	@GraphQLQuery(name = "cd_cest")
+	@JsonProperty(value = "Cest_Codigo")
 	private String cest;
 	
+	@JsonProperty(value = "Nome")
 	@GraphQLQuery(name = "ds_produto")
 	private String description;
 	
+	@JsonProperty(value = "QuantidadeEmbalagem")
 	@GraphQLQuery(name = "qt_embalagem")
 	private String quantityInPackage;
 }
