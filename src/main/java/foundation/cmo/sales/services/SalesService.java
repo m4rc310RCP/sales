a56b13aeafc3b7261be27938be2f9eb362ef2d20
Marcelo <mlsilva@mlsilva.local>
2023-01-05 22:01:02 -0300

    Testes de deploy dokku

--- src/main/java/foundation/cmo/sales/services/SalesService.java
+++ src/main/java/foundation/cmo/sales/services/SalesService.java
@@ -8,8 +8,12 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Service
 @GraphQLApi
 public class SalesService {
+	
+	public final String INFO = "Teste de anotations";
+	
+	
 
-	@GraphQLQuery(name = "test", description = "${info.4.test}")
+	@GraphQLQuery(name = "test", description = INFO)
 	public String testService() {
 		return "OK";
 	}
